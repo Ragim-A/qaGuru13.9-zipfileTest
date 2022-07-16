1f1fce431cf563c8917748861db7af14c6e402be
Ragim <ratutnetu@gmail.com>
2022-07-16 21:11:52 +0300

    Исправлены файлы CSV, XLSX. Test successful.

--- src/test/java/ZipFileTest.java
+++ src/test/java/ZipFileTest.java
@@ -5,23 +5,15 @@ import org.junit.jupiter.api.Test;
 import com.codeborne.pdftest.PDF;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
-import java.util.zip.ZipInputStream;
-
 import static com.codeborne.pdftest.assertj.Assertions.assertThat;
-import static java.nio.charset.StandardCharsets.UTF_8;
-
 public class ZipFileTest {
     @Test
     @DisplayName("Zip Files Test")
     void ZipTest() throws Exception {
-        ZipFile zip = new ZipFile("zipTest.zip");
+        ZipFile zip = new ZipFile("src/test/resources/zipTest.zip");
 
         ZipEntry csv = new ZipEntry("list.csv");
         try (InputStream csvStream = zip.getInputStream(csv)) {
@@ -41,7 +33,7 @@ public class ZipFileTest {
         ZipEntry XlsEntry = zip.getEntry("sale.xlsx");
         try (InputStream xlsStream = zip.getInputStream(XlsEntry)) {
             XLS parsed = new XLS(xlsStream);
-            assertThat(parsed.excel.getSheetAt(0).getRow(1).getCell(1).getStringCellValue())
+            assertThat(parsed.excel.getSheetAt(0).getRow(1).getCell(0).getStringCellValue())
                     .isEqualTo("two");
         }
     }
